d7d8ea782fb37605b2f2d44c1a44a632c5b4bfa9
Doublecouponday <mail@samueljenks.me>
2026-02-17 16:31:55 +1300

    wip: linking with omron builtins

--- build.ps1
+++ build.ps1
@@ -12,16 +12,16 @@ if($LASTEXITCODE -ne 0) {
 }
 
 # clampandsaw
-plc ./source/* -c -l iec61131std -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.o
+plc ./source/* -c -l iec61131std -l libomron -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.o
 
 if($LASTEXITCODE -ne 0) {
     exit
 }
 
-clang ./compiled/lib_structured_text.o --shared -l iec61131std -l ws2_32 -l ntdll -l userenv -fuse-ld=lld-link "-Wl,/DEF:exports.def" -o ./compiled/lib_structured_text.dll
+clang ./compiled/lib_structured_text.o --shared -l iec61131std -l libomron -l ws2_32 -l ntdll -l userenv -fuse-ld=lld-link "-Wl,/DEF:exports.def" -o ./compiled/lib_structured_text.dll
 
 if($LASTEXITCODE -ne 0) {
     exit
 }
 
-plc ./source/clampandsaw.st --xml-omron -i ./source/externals.st -i ./source/omron.st -l iec61131std -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.xml
+plc ./source/clampandsaw.st --xml-omron -i ./source/externals.st -i ./source/omron.st -l iec61131std -l libomron -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.xml
