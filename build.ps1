a93f2f94e8e205938e7b57af2e140ab2841fc6a3
Doublecouponday <mail@samueljenks.me>
2026-02-20 10:44:56 +1300

    step: discovered successful builtin library linking pathway

--- build.ps1
+++ build.ps1
@@ -1,27 +1,27 @@
 # libomron
-plc ./libomron/*.st -c -l iec61131std -l ws2_32 -l ntdll -l userenv -o ./compiled/libomron.o
+plc ./libomron/*.st -c -l iec61131std -l ws2_32 -l ntdll -l userenv -o ./compiled/libNX1P2.o
 
 if($LASTEXITCODE -ne 0) {
     exit
 }
 
-clang ./compiled/libomron.o --shared -l iec61131std -l ws2_32 -l ntdll -l userenv -fuse-ld=lld-link "-Wl,/DEF:libomron/exports.def" -o ./compiled/libomron.dll
+clang ./compiled/libNX1P2.o --shared -l iec61131std -l ws2_32 -l ntdll -l userenv -fuse-ld=lld-link "-Wl,/DEF:libomron/exports.def" -o ./compiled/libNX1P2.dll
 
 if($LASTEXITCODE -ne 0) {
     exit
 }
 
 # clampandsaw
-plc ./source/*.st -c -i ./externals/stdlib_externals.st -i ./externals/omron_externals.st -l iec61131std -l libomron -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.o
+plc ./source/*.st -c -i ./externals/stdlib_externals.st -i ./externals/omron_externals.st -l iec61131std -l libNX1P2 -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.o
 
 if($LASTEXITCODE -ne 0) {
     exit
 }
 
-clang ./compiled/lib_structured_text.o --shared -l iec61131std -l libomron -l ws2_32 -l ntdll -l userenv -fuse-ld=lld-link "-Wl,/DEF:exports.def" -o ./compiled/lib_structured_text.dll
+clang ./compiled/lib_structured_text.o --shared -l iec61131std -l libNX1P2 -l ws2_32 -l ntdll -l userenv -fuse-ld=lld-link "-Wl,/DEF:exports.def" -o ./compiled/lib_structured_text.dll
 
 if($LASTEXITCODE -ne 0) {
     exit
 }
 
-plc ./source/clampandsaw.st ./source/testallbuiltins.st --xml-omron -i ./externals/stdlib_externals.st -i ./externals/omron_externals.st -l iec61131std -l libomron -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.xml
+plc ./source/clampandsaw.st ./source/testallbuiltins.st --xml-omron -i ./externals/stdlib_externals.st -i ./externals/omron_externals.st -l iec61131std -l libNX1P2 -l ws2_32 -l ntdll -l userenv -o ./compiled/lib_structured_text.xml
