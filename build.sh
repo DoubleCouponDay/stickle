747201c47b144e5b81cd05ccad030904ba910304
DoubleCouponDay <{ID}+{username}@users.noreply.github.com>
2026-08-30 13:31:15 +1000

    fix: linux build script

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # libNX1P2
-plc ./libNX1P2/*.st --shared --linker=cc --target=x86_64 -l iec61131std -o ./compiled/libNX1P2.so
+plc ./libNX1P2/*.st --shared --linker=cc -l iec61131std -o ./compiled/libNX1P2.so
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 # clampandsaw
-plc ./source/*.st --shared --linker=cc --target=x86_64 -i ./externals/stdlib_externals.st -i ./externals/omron_externals.st -L ./compiled -l iec61131std -l NX1P2 -o ./compiled/lib_structured_text.so
+plc ./source/*.st --shared --linker=cc -i ./externals/stdlib_externals.st -i ./externals/omron_externals.st -L ./compiled -l iec61131std -l NX1P2 -o ./compiled/lib_structured_text.so
 
 if [ $? -ne 0 ]; then
     exit 1
